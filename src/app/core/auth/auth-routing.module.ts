1f314fb5d67d6ebc473f147cf3b4097916ff6ba4
Musawir <musawir@nkutechnologies.com>
2022-07-31 15:26:41 +0500

    another login page added

--- src/app/core/auth/auth-routing.module.ts
+++ src/app/core/auth/auth-routing.module.ts
@@ -1,3 +1,4 @@
 import { LoginComponent } from './login/login.component';
+import { SignInComponent } from './sign-in/sign-in.component';
 
-export const Routes = [{ path: '', component: LoginComponent }];
+export const Routes = [{ path: '', component: LoginComponent },{ path: 'login', component: SignInComponent },];
--- src/app/core/auth/auth.module.ts
+++ src/app/core/auth/auth.module.ts
@@ -4,8 +4,9 @@ import { AuthComponent } from './auth.component';
 import { LoginComponent } from './login/login.component';
 import { RouterModule } from '@angular/router';
 import { Routes } from './auth-routing.module';
+import { SignInComponent } from './sign-in/sign-in.component';
 @NgModule({
   imports: [CommonModule, RouterModule.forChild(Routes)],
-  declarations: [AuthComponent, LoginComponent],
+  declarations: [AuthComponent, LoginComponent, SignInComponent],
 })
 export class AuthModule {}
