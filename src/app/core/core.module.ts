a5bf94636aeb6d2882959cb64884680ed3664a6a
mumarqayyum051 <mumarqayyum051>
2022-07-31 21:50:48 +0500

    services added

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,10 +3,16 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '../shared/shared.module';
 import { LayoutComponent } from './layout/layout.component';
 import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { ApiService } from './services/api.service';
+import { AuthGuard } from './services/auth-guard.service';
+import { JwtService } from './services/jwt.service';
+import { NoAuthGuard } from './services/no-auth-gaurd.service';
+import { UserService } from './services/user.service';
 
 @NgModule({
   imports: [CommonModule, SharedModule],
   declarations: [SidebarComponent, LayoutComponent],
   exports: [LayoutComponent],
+  providers: [UserService, ApiService, JwtService, AuthGuard, NoAuthGuard],
 })
 export class CoreModule {}
---
+++ src/app/core/services/no-auth-gaurd.service.ts
@@ -0,0 +1,39 @@
+import { UserService } from 'src/app/core/services/user.service';
+import { Injectable } from '@angular/core';
+import {
+  ActivatedRouteSnapshot,
+  CanActivate,
+  Router,
+  RouterStateSnapshot,
+} from '@angular/router';
+
+import { take } from 'rxjs/operators';
+import { JwtService } from './jwt.service';
+
+@Injectable()
+export class NoAuthGuard implements CanActivate {
+  constructor(
+    private userService: UserService,
+    private router: Router,
+    private jwtService: JwtService
+  ) {}
+
+  canActivate(
+    route: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): boolean {
+    if (this.jwtService.getToken()) {
+      let user = this.userService.getCurrentUser();
+      console.log('**', user);
+
+      if (user.isEmailVerified) {
+        this.router.navigate(['/discover']);
+      } else {
+        this.userService.purgeAuth();
+        this.router.navigate(['/auth/verify']);
+      }
+      return false;
+    }
+    return true;
+  }
+}
